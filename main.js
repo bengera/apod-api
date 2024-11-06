99d000a7a84a6fa51669550422a4118298ef4d52
Ben <61493854+bengera@users.noreply.github.com>
2024-11-06 13:30:43 +0700

    demo key

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 'use strict';
 
 const imageContainer = document.querySelector('.image-container');
-
+const explanationText = document.querySelector('.explanation');
 
 const getImage = function(){
     return new Promise(function(resolve, reject){
         fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
         .then(res =>{
             console.log(res);
-        })
+            if(!res.ok){
+                reject(new Error('Data not found'))
+                return
+            }
+            return res.json();
+        }).then(data => {
+            console.log(data);
+            resolve(data);
+        }).catch((err) => console.log(`${err}`))
     })
 }
 
 
-getImage();
+
+// date
+// explanation
+//title
+//url
+//copyright
+
+getImage()
+.then((data)=>{
+    console.log(`${data.date}:This is ${data.title}, ${data.copyright}`)
+    explanationText.textContent = data.explanation;
+    const apodimg = `${data.url}`
+    const img = document.createElement('img');
+    img.src = apodimg;
+    imageContainer.appendChild(img)
+
+
+})
