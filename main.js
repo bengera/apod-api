4941bb6ee9f87dc37cbb89258b5c3f8bcc4b0ada
Ben <61493854+bengera@users.noreply.github.com>
2024-11-06 19:28:15 +0700

    add styling

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 
 const imageContainer = document.querySelector('.image-container');
 const explanationText = document.querySelector('.explanation');
+const heading = document.querySelector('.heading');
 
 const getImage = function(){
     return new Promise(function(resolve, reject){
-        fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        fetch('https://api.nasa.gov/planetary/apod?api_key=jXbNGXKLCfz4KjuUikeHoDOXlAbKoApEXgGKsKH2')
         .then(res =>{
             console.log(res);
             if(!res.ok){
@@ -22,20 +23,16 @@ const getImage = function(){
 
 
 
-// date
-// explanation
-//title
-//url
-//copyright
-
 getImage()
 .then((data)=>{
     console.log(`${data.date}:This is ${data.title}, ${data.copyright}`)
     explanationText.textContent = data.explanation;
+    heading.textContent = data.title
     const apodimg = `${data.url}`
     const img = document.createElement('img');
-    img.src = apodimg;
+    img.classList.add('image')
+;   img.src = apodimg;
     imageContainer.appendChild(img)
 
 
-})
+}).catch((err) => console.log(`${err}`));
